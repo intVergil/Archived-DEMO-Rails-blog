5b1be56ae1efb110fecde89da54995e73fd82559
intvergil <yan53074@gmail.com>
2018-07-03 23:11:38 +0900

    add markdown

--- Gemfile
+++ Gemfile
@@ -31,6 +31,10 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# Use Markdown
+gem 'redcarpet', '~> 3.4'
+gem 'rouge', '~> 3.1', '>= 3.1.1'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,17 @@
 class BlogsController < ApplicationController
   def show
+    @article="> #####ブール値の命名
+>
+> 真偽値を返す関数またはブール変数の名前を選択するときは、真偽が本当に意味するものが明確であることを確認してください。
+>
+> ここに危険な例があります：
+> 	bool read_password = true;
+>
+> あなたがそれをどのように読んでいるか（意図していない）に応じて、2つの全く異なる解釈があります：
+>
+> - パスワードを読む必要があります
+> - パスワードはすでに読み込まれています
+>
+> この場合、「読み込み」という単語を避け、need_passwordまたはuser_is_authenticatedという名前を付けてください。"
   end
 end
